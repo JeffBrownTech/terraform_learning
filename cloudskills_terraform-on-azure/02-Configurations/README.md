96b41f4996a2d762b5af190234c4396a22bb644a
Jeff Brown Tech <jeff@jeffbrown.tech>
2021-02-14 03:27:33 +0000

    module 2 challenge

---
+++ cloudskills_terraform-on-azure/02-Configurations/README.md
@@ -0,0 +1,11 @@
+# End of Module Challenge
+
+Create a Terraform configuration file to provision:
+
+- Resource Group
+- Virtual Network
+- Subnet
+- Network Interface
+- Virtual Machine
+-- Windows Server 2016 Datacenter
+-- Size: Standard_B1s
---
+++ cloudskills_terraform-on-azure/02-Configurations/main.tf
@@ -0,0 +1,75 @@
+# Terraform
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "2.40.0"
+     }
+  }
+}
+
+#Azure provider
+provider "azurerm" {
+  features {}
+}
+
+#create resource group
+resource "azurerm_resource_group" "rg" {
+    name     = "rg-terrformchallenge"
+    location = "westus2"
+    tags      = {
+      Environment = "terraexample"
+    }
+}
+
+#Create virtual network
+resource "azurerm_virtual_network" "vnet" {
+    name                = "vnet-dev-westus2-001"
+    address_space       = ["10.1.0.0/16"]
+    location            = azurerm_resource_group.rg.location
+    resource_group_name = azurerm_resource_group.rg.name
+}
+
+# Create subnet
+resource "azurerm_subnet" "subnet" {
+  name                 = "snet-dev-westus2-001"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.1.0.0/24"]
+}
+
+# Create a network interface
+resource "azurerm_network_interface" "nic" {
+  name                = "nic-01-vm01-dev-001"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+  # Create a virtual machine
+resource "azurerm_windows_virtual_machine" "vm" {
+  name                  = "vm01"
+  resource_group_name   = azurerm_resource_group.rg.name
+  location              = azurerm_resource_group.rg.location
+  size                  = "Standard_B1s"
+  admin_username        = "adminuser"
+  admin_password        = "P@$$w0rd1234!" # not a real password or best practice!
+  network_interface_ids = [azurerm_network_interface.nic.id]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2016-Datacenter"
+    version   = "latest"
+  }
+}
